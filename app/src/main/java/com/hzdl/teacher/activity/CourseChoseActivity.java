01d32beab1a29058ad1e0d8f9a53dae33c3a5192
kaka <125921384@qq.com>
2017-09-28 14:49:37 +0800

    no message

---
+++ app/src/main/java/com/hzdl/teacher/activity/CourseChoseActivity.java
@@ -0,0 +1,145 @@
+package com.hzdl.teacher.activity;
+
+import android.content.Intent;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.os.Message;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.hzdl.teacher.R;
+import com.hzdl.teacher.base.BaseActivity;
+
+public class CourseChoseActivity extends BaseActivity {
+
+    private RecyclerView rcy_course;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_chose);
+
+        initRcy();
+    }
+
+    private void initRcy() {
+
+        rcy_course = (RecyclerView)findViewById(R.id.rcy_course);
+        //设置布局管理器
+        rcy_course.setLayoutManager(new GridLayoutManager(this,5));
+        CCAdapter cc = new CCAdapter();
+        cc.setOnItemClickLitener(new OnItemClickLitener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Intent intent=new Intent(CourseChoseActivity.this,CourseActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onItemLongClick(View view, int position) {
+
+            }
+        });
+        rcy_course.setAdapter(cc);
+        rcy_course.setItemAnimator(new DefaultItemAnimator());
+        rcy_course.addItemDecoration(new SpaceItemDecoration(50));
+
+
+    }
+
+    public interface OnItemClickLitener
+    {
+        void onItemClick(View view, int position);
+        void onItemLongClick(View view , int position);
+    }
+
+    class CCAdapter extends RecyclerView.Adapter<CCAdapter.MyViewHolder>
+    {
+
+
+        private OnItemClickLitener mOnItemClickLitener;
+
+        public void setOnItemClickLitener(OnItemClickLitener mOnItemClickLitener)
+        {
+            this.mOnItemClickLitener = mOnItemClickLitener;
+        }
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+        {
+
+            View v = LayoutInflater.from(CourseChoseActivity.this).inflate(R.layout.item_c_c, parent, false);
+            final MyViewHolder holder = new MyViewHolder(v);
+
+            if(mOnItemClickLitener!=null){
+                v.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        int pos = holder.getLayoutPosition();
+                        mOnItemClickLitener.onItemClick(holder.itemView, pos);
+                    }
+                });
+            }
+
+            return holder;
+        }
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position)
+        {
+            holder.tv_name.setText("第 " + (position+1) + " 课");
+        }
+        @Override
+        public int getItemCount()
+        {
+            return 32;
+        }
+        class MyViewHolder extends RecyclerView.ViewHolder
+        {
+            ImageView tv;
+            TextView tv_name;
+            public MyViewHolder(View view)
+            {
+                super(view);
+                tv = (ImageView) view.findViewById(R.id.imageView);
+                tv_name = (TextView) view.findViewById(R.id.tv_name);
+            }
+        }
+    }
+
+    class SpaceItemDecoration extends RecyclerView.ItemDecoration {
+
+        int mSpace;
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            super.getItemOffsets(outRect, view, parent, state);
+
+            outRect.left = mSpace;
+            outRect.right = mSpace;
+            outRect.bottom = mSpace;
+            outRect.top = 10;
+
+        }
+
+        public SpaceItemDecoration(int space) {
+            this.mSpace = space;
+        }
+    }
+
+    @Override
+    protected void handleMsg(Message msg) {
+
+        
+        
+    }
+
+
+
+}
