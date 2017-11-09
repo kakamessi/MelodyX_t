0552ede7240f5d182ca8bedbc0db90a827761357
kaka <125921384@qq.com>
2017-11-09 11:40:08 +0800

    no message

--- mex/src/main/java/com/hzdl/mex/socket/SocketParams.java
+++ mex/src/main/java/com/hzdl/mex/socket/SocketParams.java
@@ -8,13 +8,13 @@ public class SocketParams {
 
 
     //服务端tcp绑定端口
-    public static final int PORT = 20000;
+    public static final int PORT = 20000 + 1;
     //服务端udp发送参数
     public static final String UDP_HOST = "255.255.255.255";
     //服务端udp发送参数
-    public static final int UDP_PORT = 8000;
+    public static final int UDP_PORT = 8000 + 1;
     //服务端udp监听端口
-    public static final int TEACHER_UDP_PORT = 8001;
+    public static final int TEACHER_UDP_PORT = 8001 + 1;
 
 
     public static final String HOST = "192.168.2.146";
