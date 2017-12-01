1a97da96d5ec7387451b86c3f2a1613e716ef6f0
kaka <125921384@qq.com>
2017-12-01 20:08:49 +0800

    no message

--- app/src/main/java/com/hzdl/teacher/activity/H5Activity.java
+++ app/src/main/java/com/hzdl/teacher/activity/H5Activity.java
@@ -1,6 +1,7 @@
 package com.hzdl.teacher.activity;
 
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Message;
 import android.view.KeyEvent;
@@ -12,6 +13,9 @@ import android.webkit.WebViewClient;
 
 import com.hzdl.teacher.R;
 import com.hzdl.teacher.base.BaseActivity;
+
+import java.util.HashMap;
+import java.util.Set;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -50,8 +54,32 @@ public class H5Activity extends BaseActivity {
         mWebview.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
+
+                // 步骤2：根据协议的参数，判断是否是所需要的url
+                // 一般根据scheme（协议格式） & authority（协议名）判断（前两个参数）
+                //假定传入进来的 url = "js://webview?arg1=111&arg2=222"（同时也是约定好的需要拦截的）
+
+                Uri uri = Uri.parse(url);
+                // 如果url的协议 = 预先约定的 js 协议
+                // 就解析往下解析参数
+                if ( uri.getScheme().equals("js")) {
+
+                    // 如果 authority  = 预先约定协议里的 webview，即代表都符合约定的协议
+                    // 所以拦截url,下面JS开始调用Android需要的方法
+                    if (uri.getAuthority().equals("webview")) {
+
+                        //  步骤3：
+                        // 执行JS所需要调用的逻辑
+                        System.out.println("js调用了Android的方法");
+                        // 可以在协议上带有参数并传递到Android上
+                        HashMap<String, String> params = new HashMap<>();
+                        Set<String> collection = uri.getQueryParameterNames();
+
+                    }
+
+                    return true;
+                }
+                return super.shouldOverrideUrlLoading(view, url);
             }
         });
         //设置WebChromeClient类
