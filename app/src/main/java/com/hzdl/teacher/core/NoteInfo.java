99ed0079b984d175fcb83ab7158934a9477a37aa
kaka <125921384@qq.com>
2018-02-09 11:46:48 +0800

    no message

--- app/src/main/java/com/hzdl/teacher/core/NoteInfo.java
+++ app/src/main/java/com/hzdl/teacher/core/NoteInfo.java
@@ -13,7 +13,16 @@ public class NoteInfo {
     private int keyIndex;
     //显示颜色
     private boolean idNoteRed;
+    //音符是否被弹奏过
+    private boolean isUsed;
 
+    public boolean isUsed() {
+        return isUsed;
+    }
+
+    public void setUsed(boolean used) {
+        isUsed = used;
+    }
 
     //命中次数
     private int hitCount = -1;
@@ -78,6 +87,60 @@ public class NoteInfo {
         this.idNoteRed = idNoteRed;
     }
 
+    //复位
+    public void reSet(){
+        setUsed(false);
+        if(getInfo()!=null){
+            getInfo().setUsed(false);
+        }
+    }
+
+    //当前音符环节是否结束
+    public boolean isFinish(){
+        boolean result = true;
+
+        if(getInfo()!=null && !getInfo().isUsed()){
+            result = false;
+        }
+
+        if(!isUsed()){
+            result = false;
+        }
+
+        return result;
+    }
+
+    //设置已用状态
+    public void setUsedStatu(int note){
+        if(getInfo()!=null && getInfo().getNote()== note){
+            getInfo().setUsed(true);
+        }
+        if(getNote() == note){
+            setUsed(true);
+        }
+    }
+
+    //获取对应音符信息
+    public NoteInfo getNoteByid(int note){
+        if(getInfo()!=null && getInfo().getNote()==note){
+            return getInfo();
+        }
+        if(getNote() == note){
+            return this;
+        }
+        return null;
+    }
+
+    //检测对应音符是否被 弹奏过
+    public boolean checkUsed(int note){
+        boolean result = false;
+
+        if(getNoteByid(note)!=null && getNoteByid(note).isUsed()){
+            result = true;
+        }
+
+        return result;
+    }
 
 }
 
